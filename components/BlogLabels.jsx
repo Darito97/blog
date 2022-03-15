7eabcd4ab67fc224e9d73605c468425b9d871545
Darito <davidalonso991@gmail.com>
2022-03-15 09:07:47 -0600

    Agregado de estilos a componente BlogLabels

--- components/BlogLabels.jsx
+++ components/BlogLabels.jsx
@@ -1,7 +1,8 @@
+import BlogLabelsStyles from '../styles/BlogLabels.module.css'
 function BlogLabels({ labels }) {
 
   return (labels ?
-    <ul>
+    <ul className={BlogLabelsStyles.listOfLabels}>
       {
         labels.map((label, index) => <li key={label + index}>#{label}</li>)
       }
